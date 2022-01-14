82c5ee1539d9a86acdb86f819b87351266e9abe8
kna27 <krisharora27@gmail.com>
2022-01-13 23:40:11 -0500

    Add player 2 movement

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,22 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 6.5f;
     public GameObject Player1;
     public GameObject Player2;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Player1.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0);
+        Player2.transform.Translate(Input.GetAxis("Horizontal2") * Time.deltaTime * speed, 0, 0);
     }
 }
