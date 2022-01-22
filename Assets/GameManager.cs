15e3ed2d7687eb49f38d3ec823af716edc0889ed
kna27 <krisharora27@gmail.com>
2022-01-22 17:06:44 -0500

    Add temporary UI

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,17 +6,13 @@ public class GameManager : MonoBehaviour
     public static int ballHolder;
     public static int scoreOne = 0;
     public static int scoreTwo = 0;
-    /*
     [SerializeField] private Text scoreOneText;
     [SerializeField] private Text scoreTwoText;
     [SerializeField] private Text debugInfo;
-    */
     void Update()
     {
-        /*
         scoreOneText.text = scoreOne.ToString();
         scoreTwoText.text = scoreTwo.ToString();
         debugInfo.text = string.Format("Ball Holder: {0}", ballHolder);
-        */
     }
 }
